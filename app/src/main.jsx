b3c348e35e43db7ece503abeb42f591d273eb540
kneesdev <kneesdev@naver.com>
2025-12-13 21:35:41 +0200

    [add]: import tailwindcss

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 function App() {
-  return <h1>Hello from bun + ultralight</h1>;
+  return <h1 className="text-4xl font-bold text-red-500">Hello from bun + ultralight</h1>;
 }
 
 createRoot(document.getElementById("root")).render(<App />);
